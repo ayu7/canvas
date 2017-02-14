b3e0d608b609a7f9b41f04120d99c6c05b88c964
ayu7 <ayu7@stuy.edu>
2017-02-14 02:40:48 -0500

    all changes

--- canvas.js
+++ canvas.js
@@ -1,5 +1,17 @@
-var box=document.getElementById("box");
-var y=box.getContext("2d");
-y.fillStyle="red";
-y.fillRect=(50,50,100,200);
+var box= document.getElementById("box");
+var boxy=box.getContext("2d");
+var button=document.getElementById("button");
 
+var fill= function(e){
+  boxy.fillStyle="red";
+  var x = e.offsetX;
+  var y = e.offsetY;
+  boxy.fillRect(x,y,100,100);
+}
+
+var clear= function(e){
+  boxy.clearRect(0, 0, 500, 500);
+}
+
+button.addEventListener("click", clear)
+box.addEventListener("click", fill)
